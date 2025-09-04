b46c5e1e965feb0b3e005cce794116aa8c78a44c
Julius <julius.a@electronicscience.net>
2025-09-04 23:24:36 +0800

    added the student dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserInfo } from '../../models/user-info';
+import { CommonModule } from '@angular/common';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { StudentDashboardComponent } from './student-dashboard/student-dashboard.component';
+import { RecyclingDashboardComponent } from './recycling-dashboard/recycling-dashboard.component';
+
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    AdminDashboardComponent,
+    StudentDashboardComponent,
+    RecyclingDashboardComponent
+  ],
+  providers: [],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
+  public user: UserInfo | undefined;
+  
+  ngOnInit(): void {
+    let user = localStorage.getItem("user_data")
+    if (user) {
+      console.log(user)
+      this.user = JSON.parse(user)
+    }
+  }
 
 }
