4b337613a3965f19ad101e926b7c87e9d2a6cb28
Julius <julius.a@electronicscience.net>
2025-09-05 21:32:06 +0800

    added the admin dashboard

--- src/app/pages/dashboard/admin-dashboard/admin-dashboard.component.ts
+++ src/app/pages/dashboard/admin-dashboard/admin-dashboard.component.ts
@@ -1,12 +1,112 @@
 import { Component } from '@angular/core';
+import { FlexLayoutModule } from "@ngbracket/ngx-layout";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from "@angular/material/icon";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
+import { MatButtonModule } from "@angular/material/button";
+import { BaseChartDirective } from 'ng2-charts';
+import { ChartConfiguration } from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+import { MatDividerModule } from "@angular/material/divider";
+
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatIconModule,
+    MatProgressBarModule,
+    MatButtonModule,
+    BaseChartDirective,
+    MatDividerModule
+],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.scss'
 })
 export class AdminDashboardComponent {
+  public data = {
+    total_bottles: 10,
+    active_students: 100,
+    total_redemptions: 500,
+    recycling_impact: "5",
+    waste_diverted: "0.3 kg",
+    flood_risk: "13%",
+    machine: {
+      is_active: true,
+      name: "CEIT Veranda",
+      level: 70,
+      occupied_charger: 2,
+      total_charger: 5,
+      last_update: "09/05/2025 21:20"
+    }
+  }
 
+  public doughnutChartLabels: string[] = [ 'Waste (kg)', 'School Chairs', 'CO2 Reduction' ];
+  public doughnutChartDatasets: ChartConfiguration<'doughnut'>['data']['datasets'] = [
+      { 
+        data: [ 350, 450, 100 ],
+        backgroundColor: ['#3B82F6', "#10B981", '#6366F1']
+      }
+    ];
+
+  public doughnutChartOptions: ChartConfiguration<'doughnut'>['options'] = {
+    responsive: true,
+    maintainAspectRatio: true,
+    plugins: {
+      legend: { position: 'top' },
+      datalabels: {
+        color: '#fff',
+        formatter: (value, ctx) => {
+          const dataset = ctx.chart.data.datasets[0].data as number[];
+          const total = dataset.reduce((acc, val) => acc + val, 0);
+          const percentage = ((value / total) * 100).toFixed(1) + '%';
+          return percentage;
+        }
+      }
+    }
+  };
+
+  doughnutChartPlugins = [ChartDataLabels];
+
+  public barChartPlugins = [];
+
+  public barChartData: ChartConfiguration<'bar'>['data'] = {
+    labels: [ 'Jan', 'Feb', 'March', 'April', 'May', 'June', 'July' ],
+    datasets: [
+      { 
+        data: [ 65, 59, 80, 81, 56, 55, 40 ],
+        backgroundColor: ['#10B981']
+      }
+      
+    ]
+    
+  };
+
+  public barChartOptions: ChartConfiguration<'bar'>['options'] = {
+    responsive: true,
+    maintainAspectRatio: false // let it fill width & height
+  };
+
+  public getMachineStatus(level: number): { label: string, cssClass: string } {
+    if (level >= 100) {
+      return { label: 'FULL', cssClass: 'red-pill' };
+    } else if (level < 60) {
+      return { label: 'LOW', cssClass: 'green-pill' };
+    } else {
+      return { label: 'ALMOST FULL', cssClass: 'warning-pill' };
+    }
+  }
+
+  getChargerUsage(): number {
+    if (!this.data?.machine?.total_charger || this.data.machine.total_charger === 0) {
+      return 0;
+    }
+    return Math.round(
+      (this.data.machine.occupied_charger / this.data.machine.total_charger) * 100
+    );
+  }
 }
