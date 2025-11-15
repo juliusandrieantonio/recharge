d6ad4cec7f4fa975c129bb98e1c6550bad7701ef
Julius <julius.a@electronicscience.net>
2025-11-15 17:53:41 +0800

    put the firebase client config in the prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,11 @@
 export const environment = {
-  production: true,
-  firebaseConfig: {
-    apiKey: process.env['FIREBASE_API_KEY'],
-    authDomain: process.env['FIREBASE_AUTH_DOMAIN'],
-    databaseURL: process.env['FIREBASE_DATABASE_URL'],
-    projectId: process.env['FIREBASE_PROJECT_ID'],
-    storageBucket: process.env['FIREBASE_STORAGE_BUCKET'],
-    messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'],
-    appId: process.env['FIREBASE_APP_ID']
+  firebaseConfig:  {
+    apiKey: "AIzaSyD8UWQz3uL1877r2QMhzY769S5JsUgsDdc",
+    authDomain: "recharge-cc317.firebaseapp.com",
+    databaseURL: "https://recharge-cc317-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "recharge-cc317",
+    storageBucket: "recharge-cc317.firebasestorage.app",
+    messagingSenderId: "1009383318489",
+    appId: "1:1009383318489:web:08cc084690fbed6abaafc8"
   }
 };
